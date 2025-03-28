355fbf49e2ce2be407a90c35cfaca13a37fdd740
Meezzi <alswl-3016@naver.com>
2025-03-28 16:32:14 +0900

    feat: 전달받은 데이터를 AppBar title에 표시

--- lib/pages/station_list/station_list_page.dart
+++ lib/pages/station_list/station_list_page.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
 
 class StationListPage extends StatelessWidget {
+  final String title;
+
+  StationListPage(this.title);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(''),
+        title: Text('$title'),
       ),
     );
   }
