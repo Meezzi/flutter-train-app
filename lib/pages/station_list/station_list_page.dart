b27ffe06139204acbcc645f6a68e9cbc384836b8
Meezzi <alswl-3016@naver.com>
2025-03-28 16:31:34 +0900

    design: AppBar 생성

--- lib/pages/station_list/station_list_page.dart
+++ lib/pages/station_list/station_list_page.dart
@@ -3,6 +3,10 @@ import 'package:flutter/material.dart';
 class StationListPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(''),
+      ),
+    );
   }
 }
