83646e07163ce7bf146f063c340bc685e98903c2
Meezzi <alswl-3016@naver.com>
2025-03-28 13:07:22 +0900

    feat: 배경 색상 변경

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -1,17 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_train_app/pages/widgets/station_box.dart';
 
-class HomePage extends StatefulWidget {
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('기차 예매'),
       ),
+      backgroundColor: Colors.grey[200],
     );
   }
 }
