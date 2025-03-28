066812b0c411a13afd3b4bb49cbb45e9409db12e
Meezzi <alswl-3016@naver.com>
2025-03-28 17:49:39 +0900

    design: StationItem UI 구현

--- lib/pages/station_list/station_list_page.dart
+++ lib/pages/station_list/station_list_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_train_app/data/station.dart';
 
 class StationListPage extends StatelessWidget {
   final String title;
@@ -10,6 +11,28 @@ class StationListPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('$title'),
+      ),
+    );
+  }
+
+  Widget StationItem(String station) {
+    return SizedBox(
+      height: 50,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Text(
+              station,
+              style: const TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
