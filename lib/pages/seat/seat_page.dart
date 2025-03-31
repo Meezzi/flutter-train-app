8e650bcc773b3ba59de8af832fbd2797a6c93564
Meezzi <alswl-3016@naver.com>
2025-03-31 14:21:21 +0900

    design: 출발역, 도착역 표시

--- lib/pages/seat/seat_page.dart
+++ lib/pages/seat/seat_page.dart
@@ -13,8 +13,13 @@ class SeatPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('좌석 선택')),
-    );
+        appBar: AppBar(title: Text('좌석 선택')),
+        body: Row(
+          children: [
+            stationTitle(arrival),
+            stationTitle(departure),
+          ],
+        ));
   }
 
   Widget stationTitle(String station) {
