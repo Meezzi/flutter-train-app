c6810cda27daa77196567d56d3261909c694da8f
Meezzi <alswl-3016@naver.com>
2025-03-31 11:05:47 +0900

    feat: HomePage에서 출발역과 도착역 상태를 저장하는 변수 선언

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -10,6 +10,10 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+
+  String? _arrival;
+  String? _departure;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
